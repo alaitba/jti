74d8aa6d8fe6cc3250a17483cf5e51ed06119665
Alexander Kuznetsov <skritku@gmail.com>
2020-01-20 22:52:13 +0200

    fix active menu item

--- config/navigation.php
+++ config/navigation.php
@@ -9,7 +9,7 @@ return [
                     'is_tree' => false,
                     'title' => 'Администраторы',
                     'route_name' => 'admin.admins',
-                    'item_active_on' => 'admin/admins*',
+                    'item_active_on' => 'admins*',
                     'icon' => 'la la-users',
                     'roles' => [
                         'admin'
@@ -19,7 +19,7 @@ return [
                     'is_tree' => false,
                     'title' => 'Призы',
                     'route_name' => 'admin.rewards.index',
-                    'item_active_on' => 'admin/rewards*',
+                    'item_active_on' => 'rewards*',
                     'icon' => 'la la-gift',
                     'roles' => [
                         'admin'
@@ -38,7 +38,7 @@ return [
                     'is_tree' => false,
                     'title' => 'План/факт закупа',
                     'route_name' => 'admin.reports.sales_plan.index',
-                    'item_active_on' => 'admin/reports/sales-plan*',
+                    'item_active_on' => 'reports/sales-plan*',
                     'icon' => 'la la-bar-chart',
                     'roles' => [
                         'admin'
