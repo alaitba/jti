5bae77a914b77cc49b043acb31e1e351f72b192f
Alexander Kuznetsov <skritku@gmail.com>
2020-03-18 21:34:27 +0200

    fix poll report; fix quiz/poll check

--- resources/views/reports/polls/view.blade.php
+++ resources/views/reports/polls/view.blade.php
@@ -1,4 +1,16 @@
 @foreach($items as $item)
-<b>{{ $item['question'] }}</b><br />
-<pre>{{ $item['answer'] }}</pre>
+    <div class="card mb-3">
+        <div class="card-header">{{ $item['question'] }}</div>
+        <div class="card-body">
+            @if(is_string($item['answer']) || count($item['answer']) == 1)
+                {{ $item['answer'] }}
+            @else
+                <ul>
+                @foreach($item['answer'] as $answer)
+                    <li>{{ $answer->getTranslation('answer', 'ru') }}</li>
+                @endforeach
+                </ul>
+            @endif
+        </div>
+    </div>
 @endforeach
