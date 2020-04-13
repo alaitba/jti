d54323fba11037e530ec13279a5318f367a22a34
Alexander Kuznetsov <skritku@gmail.com>
2020-04-13 19:38:20 +0300

    partners report: status filter, datatables update

--- resources/views/reports/partners/index.blade.php
+++ resources/views/reports/partners/index.blade.php
@@ -21,6 +21,17 @@
                 <th>Последний вход</th>
                 <th>Статус</th>
             </tr>
+            <tr class="nowrap">
+                <th colspan="5"></th>
+                <th>
+                    <select class="form-control form-control-sm" id="statusFilter">
+                        <option value="">Все</option>
+                        <option value="Verified">Verified</option>
+                        <option value="Not verified">Not verified</option>
+                        <option value="Deleted">Deleted</option>
+                    </select>
+                </th>
+            </tr>
             </thead>
             <tbody>
 
@@ -30,7 +41,9 @@
 </div>
 @stop
 @push('modules')
-    <script src="/core/adminLTE/assets/vendors/custom/datatables/datatables.bundle.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/v/bs4/jszip-2.5.0/dt-1.10.20/af-2.3.4/b-1.6.1/b-colvis-1.6.1/b-flash-1.6.1/b-html5-1.6.1/b-print-1.6.1/cr-1.5.2/fc-3.3.0/fh-3.1.6/kt-2.5.1/r-2.2.3/rg-1.1.1/rr-1.2.6/sc-2.0.1/sp-1.0.1/sl-1.3.1/datatables.min.js"></script>
     <script>
         $(document).ready( function () {
             $('#partnersTable').DataTable({
@@ -57,13 +70,19 @@
                         extend: 'csv',
                         filename: 'Зарегистрированные продавцы',
                     }
-                ]
+                ],
+                initComplete: function () {
+                    $('#statusFilter').on('change', e => {
+                        const val = $(e.currentTarget).val();
+                        $('#partnersTable').dataTable().api().column(5).search(val ? `^${val}$` : '', true, false).draw();
+                    });
+                }
             });
         } );
     </script>
 @endpush
 @push('css')
-    <link href="/core/adminLTE/assets/vendors/custom/datatables/datatables.bundle.css" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs4/jszip-2.5.0/dt-1.10.20/af-2.3.4/b-1.6.1/b-colvis-1.6.1/b-flash-1.6.1/b-html5-1.6.1/b-print-1.6.1/cr-1.5.2/fc-3.3.0/fh-3.1.6/kt-2.5.1/r-2.2.3/rg-1.1.1/rr-1.2.6/sc-2.0.1/sp-1.0.1/sl-1.3.1/datatables.min.css"/>
     <style type="text/css">
         table.dataTable th:before, table.dataTable th:after {
             font-size: 1.5em !important;
