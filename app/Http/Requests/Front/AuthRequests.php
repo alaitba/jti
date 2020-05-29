20c2248602ba803278fcdc7bd1a6292efde6d5e4
Avutzhan Dautov <dautov92@list.ru>
2020-05-29 18:35:20 +0600

    Captcha validation bug fixed

--- app/Http/Requests/Front/AuthRequests.php
+++ app/Http/Requests/Front/AuthRequests.php
@@ -12,7 +12,7 @@ class AuthRequests
 {
     public const PHONE_REQUEST = [
         'mobile_phone' => 'required|regex:/77[0-9]{9}/',
-        'captcha' => 'required|captcha',
+        'captcha' => 'required',
     ];
 
     public const SMSCODE_REQUEST = [
