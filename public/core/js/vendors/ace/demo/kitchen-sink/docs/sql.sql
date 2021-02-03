abebe6f88beeb1ed1b67aba3b25a18fa6185a90b
Avutzhan Dautov <dautov92@list.ru>
2021-02-03 11:04:52 +0600

    database dumps from prod

--- public/core/js/vendors/ace/demo/kitchen-sink/docs/sql.sql
+++ public/core/js/vendors/ace/demo/kitchen-sink/docs/sql.sql
@@ -1,6 +1,3 @@
-SELECT city, COUNT(id) AS users_count
-FROM users
-WHERE group_name = 'salesman'
-AND created > '2011-05-21'
-GROUP BY 1
-ORDER BY 2 DESC
+version https://git-lfs.github.com/spec/v1
+oid sha256:e12d08c436c547e37b7def4165ac7538d759aea6fe6e6ff3a62fe8551c122a59
+size 132
